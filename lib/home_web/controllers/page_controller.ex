a8708204f7bb0f7dfd8134871a021b51437294d7
Michael Ries <michael@riesd.com>
2018-09-26 20:42:29 -0600

    render a basic zone status

--- lib/home_web/controllers/page_controller.ex
+++ lib/home_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule HomeWeb.PageController do
   use HomeWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    zones = Home.Zones.get()
+    render conn, "index.html", zones: zones
   end
 end
