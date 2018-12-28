319ab51d2b8d6d49d8e54c431b7b3eb20c2b8443
Michael Ries <michael@riesd.com>
2018-12-27 23:03:53 -0700

    remove unused gnat configuration

--- config/config.exs
+++ config/config.exs
@@ -26,22 +26,6 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:user_id]
 
-config :home, :gnat_connection, %{
-    name: :gnat,
-    connection_settings: [
-      %{host: 'nats.riesd.com', port: 4223, tls: true, username: System.get_env("NATS_USER"), password: System.get_env("NATS_PASS")},
-    ]
-  }
-
-config :home, :gnat_consumer, %{
-    connection_name: :gnat,
-    consuming_function: {Home.Zones, :accept_update},
-    subscription_topics: [
-      %{topic: "sprinkler.zones.*", queue_group: "#{Mix.env}.home.riesd.com"},
-    ],
-  }
-
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
